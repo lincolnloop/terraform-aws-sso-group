dc4bdda36a2245396ac7759e01b9191215c21a84
dtorresf <danielatorres@danielatorres.me>
2023-05-25 18:05:30 -0500

    Fixing format with terraform fmt

--- main.tf
+++ main.tf
@@ -32,10 +32,8 @@ resource "aws_ssoadmin_account_assignment" "this" {
   for_each           = toset(var.target_account_ids)
   instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
   permission_set_arn = aws_ssoadmin_permission_set.this.arn
-
-  principal_id   = aws_identitystore_group.this.group_id
-  principal_type = "GROUP"
-
-  target_id   = each.key
-  target_type = "AWS_ACCOUNT"
+  principal_id       = aws_identitystore_group.this.group_id
+  principal_type     = "GROUP"
+  target_id          = each.key
+  target_type        = "AWS_ACCOUNT"
 }
